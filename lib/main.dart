cb7afb3574b4b0ee8a7cb8b244c39d6f38e3813d
risheetajain <risheetajain309@gmail.com>
2023-11-27 21:20:57 +0530

    Cashfree implement successfully

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,20 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_cashfree_pg_sdk/api/cferrorresponse/cferrorresponse.dart';
 import 'package:flutter_cashfree_pg_sdk/api/cfpayment/cfdropcheckoutpayment.dart';
-import 'package:flutter_cashfree_pg_sdk/api/cfpayment/cfwebcheckoutpayment.dart';
 import 'package:flutter_cashfree_pg_sdk/api/cfpaymentcomponents/cfpaymentcomponent.dart';
 import 'package:flutter_cashfree_pg_sdk/api/cfpaymentgateway/cfpaymentgatewayservice.dart';
 import 'package:flutter_cashfree_pg_sdk/api/cfsession/cfsession.dart';
 import 'package:flutter_cashfree_pg_sdk/api/cftheme/cftheme.dart';
 import 'package:flutter_cashfree_pg_sdk/utils/cfenums.dart';
 import 'package:flutter_cashfree_pg_sdk/utils/cfexceptions.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
 
-
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  dotenv.load(fileName: "lib/.env");
   runApp(const MyApp());
 }
 
@@ -22,50 +26,95 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          displaySmall: TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
 
-  var cfPaymentGatewayService = CFPaymentGatewayService();
+        // is not restarted.
+        primarySwatch: Colors.green,
+      ),
+      home: const CashFreePayment(),
+    );
+  }
+}
+
+class CashFreePayment extends StatefulWidget {
+  const CashFreePayment({super.key});
+
+  @override
+  State<CashFreePayment> createState() => _CashFreePaymentState();
+}
+
+class _CashFreePaymentState extends State<CashFreePayment> {
+  CFPaymentGatewayService cfPaymentGatewayService =
+      CFPaymentGatewayService(); //Cashfree Payment Instance
+  bool? isSuccess;
 
   @override
   void initState() {
     super.initState();
+    //Attach events when payment is success and when error occured
     cfPaymentGatewayService.setCallback(verifyPayment, onError);
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Column(
-            children: [
-              TextButton(onPressed: pay, child: const Text("Pay")),
-              TextButton(onPressed: webCheckout, child: const Text("Web Checkout"))
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cashfree Payment Flutter Template'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(onPressed: pay, child: const Text("Pay")),
+            Text(
+              "Payment Status $isSuccess",
+              style: const TextStyle(fontSize: 20),
+            )
+          ],
         ),
       ),
     );
-  }
+  } //When payment is done successfully
 
   void verifyPayment(String orderId) {
-    print("Verify Payment");
+//Here we will only print the statement to check payment is done or not
+    isSuccess = true;
+    setState(() {});
+    print("Verify Payment $orderId");
   }
 
+//If some error occur during payment this will trigger
   void onError(CFErrorResponse errorResponse, String orderId) {
+    // printing the error message so that we can show it to user or  checkourselves for testing
+    isSuccess = false;
+    setState(() {});
     print(errorResponse.getMessage());
     print("Error while making payment");
   }
 
-  String orderId = "Order0001";
-  String paymentSessionId = "Tv9JCN4MzUIJiOicGbhJCLiQ1VKJiOiAXe0Jye.Fn9JSOidDNxEjZ1UWOxQjNiojI0xWYz9lIsUjN4ATMwIDO2EjOiAHelJCLiIlTJJiOik3YuVmcyV3QyVGZy9mIsEjOiQnb19WbBJXZkJ3biwiIxADMwIXZkJ3TiojIklkclRmcvJye.XnlNbPU9HGT13yeTWDR7h1JtoCIL3otJsxC7AXWKl0Xnzu6j593opohY0V67WFv_-P";
-  CFEnvironment environment = CFEnvironment.SANDBOX;
+  String orderId = "my_order_id1";
 
-  CFSession? createSession() {
+  Future<CFSession?> createSession() async {
     try {
-      var session = CFSessionBuilder().setEnvironment(environment).setOrderId(orderId).setPaymentSessionId(paymentSessionId).build();
+      final mySessionIDData = await createSessionID(
+          orderId); //This will create session id from flutter itself
+
+//Now we will se some parameter like orderID ,environment,payment sessionID after that we wil create the checkout session which will launch through which user can pay.
+      var session = CFSessionBuilder()
+          .setEnvironment(CFEnvironment.SANDBOX)
+          .setOrderId(mySessionIDData["order_id"])
+          .setPaymentSessionId(mySessionIDData["payment_session_id"])
+          .build();
       return session;
     } on CFException catch (e) {
       print(e.message);
@@ -75,30 +124,76 @@ class _MyAppState extends State<MyApp> {
 
   pay() async {
     try {
-      var session = createSession();
+      var session = await createSession();
       List<CFPaymentModes> components = <CFPaymentModes>[];
-      var paymentComponent = CFPaymentComponentBuilder().setComponents(components).build();
-
-      var theme = CFThemeBuilder().setNavigationBarBackgroundColorColor("#FF0000").setPrimaryFont("Menlo").setSecondaryFont("Futura").build();
-
-      var cfDropCheckoutPayment = CFDropCheckoutPaymentBuilder().setSession(session!).setPaymentComponent(paymentComponent).setTheme(theme).build();
+      //If you want to set paument mode to be shown to customer
+      var paymentComponent =
+          CFPaymentComponentBuilder().setComponents(components).build();
+//We will set theme of checkout session page like fonts, color
+      var theme = CFThemeBuilder()
+          .setNavigationBarBackgroundColorColor("#FF0000")
+          .setPrimaryFont("Menlo")
+          .setSecondaryFont("Futura")
+          .build();
+//Create checkout with all the settings we have set earlier
+      var cfDropCheckoutPayment = CFDropCheckoutPaymentBuilder()
+          .setSession(session!)
+          .setPaymentComponent(paymentComponent)
+          .setTheme(theme)
+          .build();
+      //Launching the payment page
 
       cfPaymentGatewayService.doPayment(cfDropCheckoutPayment);
     } on CFException catch (e) {
       print(e.message);
     }
-
   }
 
-  webCheckout() async {
-    try {
-      var session = createSession();
-      var cfWebCheckout = CFWebCheckoutPaymentBuilder().setSession(session!).build();
-      cfPaymentGatewayService.doPayment(cfWebCheckout);
-    } on CFException catch (e) {
-      print(e.message);
-    }
+  // webCheckout() async {
+  //   try {
+  //     var session = await createSession();
+  //     var cfWebCheckout =
+  //         CFWebCheckoutPaymentBuilder().setSession(session!).build();
+  //     cfPaymentGatewayService.doPayment(cfWebCheckout);
+  //   } on CFException catch (e) {
+  //     print(e.message);
+  //   }
+  // }
+}
 
+createSessionID(String orderID) async {
+  var headers = {
+    'Content-Type': 'application/json',
+    'x-client-id': dotenv.env["API_CLIENT"] ?? "",
+    'x-client-secret': dotenv.env["API_SECRET"] ?? "",
+    'x-api-version': '2022-09-01', //This is latest version for API
+    'x-request-id': 'fluterwings'
+  };
+  print(headers);
+  var request =
+      http.Request('POST', Uri.parse('https://sandbox.cashfree.com/pg/orders'));
+  request.body = json.encode({
+    "order_amount": 1, //Order Amount in Rupees
+    "order_id": orderID, //OrderiD created by you it must be unique
+    "order_currency": "INR", //Currency of order like INR,USD
+    "customer_details": {
+      "customer_id": "customer_id", //Customer id
+      "customer_name": "customer_name", //Name of customer
+      "customer_email": "flutterwings304@gmail.com", //Email id of customer
+      "customer_phone": "+917737366393" //Phone Number of customer
+    },
+    "order_meta": {"notify_url": "https://test.cashfree.com"},
+    "order_note": "some order note here" //If you want  to store something extra
+  });
+  request.headers.addAll(headers);
+
+  http.StreamedResponse response = await request.send();
+
+  if (response.statusCode == 200) {
+    //If All the details is correct it will return the response and you can get sessionid for checkout
+    return jsonDecode(await response.stream.bytesToString());
+  } else {
+    print(await response.stream.bytesToString());
+    print(response.reasonPhrase);
   }
-
 }
